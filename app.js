ec7428b6ca76e9346a60ae21421037454b112ddb
JamesKim <james.kim.nz92@gmail.com>
2017-08-30 17:43:51 +1200

    test

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var requestHandler = require('./requestHandler');
 
 //socket.io
 var server = require('http').Server(app);
-var io = require('socket.io')(server,{origins: "localhost:3002"});
+var io = require('socket.io')(server,{origins: "*:*"});
 
 io.on('connection', function(socket){
   console.log('connect sc');
